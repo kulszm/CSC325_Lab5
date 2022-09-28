1eaec9520d4b21e4a4d2649c8b8d47cd0b8c2bf7
kulsomzaraei <kulsomzaraei@localhost>
2022-09-28 12:45:28 -0400

    ToDo 3:add GPA and getters and setters for gpa

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -9,27 +9,23 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author MoaathAlrajab
  */
-public abstract class Student extends Human {
-
-    private int credits;
+public abstract class Student extends  Human {
 	// ToDo 1: Make this class a child of Human
 	
 	// ToDo 2: Fix the resulting errors
 	
-	// ToDo 3: Add a field for GPA and create setter and getter        
-        public Student(String name, short age, int credits){
-            
-            
+	// ToDo 3: Add a field for GPA and create setter and getter
+        Student(String name, short age){
+            int gpa = 0;
         }
-         public int getCredit(){
-            return credits;
+        
+        public int getGPA(int gpa){
+            return gpa;
         }
-         
-        public void setCredits (int credits){
-            this.credits = credits;
+        
+        public void setGPA(){
         }
-       
-    
+	
 	// ToDo 4: Add comments to your code
 
 }
