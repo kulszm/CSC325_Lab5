589b7bdd5a2b07471508d101721367c454184f9a
kulsomzaraei <kulsomzaraei@localhost>
2022-09-28 12:17:26 -0400

    ToDo 3: Add field for gpa (updated)

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -14,8 +14,18 @@ public abstract class Student extends Human {
 	
 	// ToDo 2: Fix the resulting errors
 	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
+	// ToDo 3: Add a field for GPA and create setter and getter        
+        public Student(String name, short age,int GPA){
+            
+            
+        }
+        
+        public void setGPA (int GPA){
+        }
+        public int setGpa (int GPA){
+            return GPA;
+        }
+    
 	// ToDo 4: Add comments to your code
 
 }
