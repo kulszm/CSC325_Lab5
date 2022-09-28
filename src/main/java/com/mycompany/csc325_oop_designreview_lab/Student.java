8595d6a083143f7d7b304d668dcbc1ce7fbd1a6b
kulsomzaraei <kulsomzaraei@localhost>
2022-09-28 13:03:16 -0400

    ToDo 6: fix student constructor to add credits

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -10,12 +10,15 @@ package com.mycompany.csc325_oop_designreview_lab;
  * @author MoaathAlrajab
  */
 public abstract class Student extends  Human {
+
+    private final int credits;
 	// ToDo 1: Make this class a child of Human
 	
 	// ToDo 2: Fix the resulting errors
 	
 	// ToDo 3: Add a field for GPA and create setter and getter
-        Student(String name, short age){
+        Student(String name, short age,int credits){
+            this.credits  = credits;
             int gpa = 0;
         }
         
